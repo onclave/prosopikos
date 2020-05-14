070a8fa7815142197aba10c1b6d019aa8eaad37d
Debabrata Acharya <sajib@Debabratas-MacBook-Pro.local>
2020-05-14 17:13:08 +0530

    change sidenav design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,23 @@ import { MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  title = 'prosopikos';
+	title: string = 'prosopikos';
 
-  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+	constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
 
-    this.matIconRegistry.addSvgIcon(
-      "github",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/image/github.svg")
-    );
-  }
+		this.matIconRegistry.addSvgIcon(
+			"github",
+			this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/image/github.svg")
+		);
+	}
 
-  go_to_github(): void {
-    (window as any).open("https://github.com/onclave", "_blank");
-  }
+	go_to_github(): void {
+		(window as any).open("https://github.com/onclave", "_blank");
+	}
 }
