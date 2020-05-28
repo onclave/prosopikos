8cdcf3d648a0efea92e8784aafcb12f19035458d
Debabrata Acharya <sajib@Debabratas-MacBook-Pro.local>
2020-05-28 09:00:53 +0530

    update news

--- src/app/coviddata.service.ts
+++ src/app/coviddata.service.ts
@@ -27,10 +27,6 @@ export class CoviddataService {
 	private COVID_19_TIMESERIES_WORLD_DEATH_URL: string = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv';
 	private COVID_19_TIMESERIES_WORLD_RECOVERY_URL: string = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv';
 
-	// private COVID_19_TIMESERIES_CONFIRMED_URL: string = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
-	// private COVID_19_TIMESERIES_DEATH_URL: string = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv';
-	// private COVID_19_TIMESERIES_RECOVERY_URL: string = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv';
-
 	private covidTimeseriesIndiaConfirmedRawData: string;
 	private covidTimeseriesIndiaRecoveryRawData: string;
 	private covidTimeseriesIndiaDeathRawData: string;
